de737717121f42c465bd3a1b2a693ce22beb2ddb
Amir Bahadori <pbahadori156@gmail.com>
2016-07-25 18:26:32 -0700

    Initial Commit

---
+++ MadLibs2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MadLibs2
+//
+//  Created by Pasha Bahadori on 7/25/16.
+//  Copyright © 2016 Pelican. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
